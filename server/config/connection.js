c3ab49a9b703a390d4f8d6a148c74efb984cb588
kumenger <44558777+kumenger@users.noreply.github.com>
2023-05-19 20:42:43 -0600

    seeding added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
+require('dotenv').config()
+connect(process.env.MONGODB_URI);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
-
-module.exports = mongoose.connection;
+module.exports = connection;
