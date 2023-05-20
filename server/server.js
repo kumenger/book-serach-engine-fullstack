3813a17aef6d97dc6de86eeaa3281a01223ae1e4
kumenger <44558777+kumenger@users.noreply.github.com>
2023-05-19 19:59:47 -0600

    typedef and reservers startup done

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {ApplloServer}=require('apollo-server-express')
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
